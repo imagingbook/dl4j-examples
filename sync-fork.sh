255031eb40ea30f9e5fb8b47adedad08c4eb1ff8
imagingbook <wilhelm.burger@fh-hagenberg.at>
2018-05-18 12:06:36 +0200

    a

--- sync-fork.sh
+++ sync-fork.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
-echo "Syncing fork to original ..."
-
-echo "Current remotes:"
-git remote -v
-
-echo "Adding original remote:"
-git remote add upstream https://github.com/deeplearning4j/dl4j-examples
-
-echo "Current remotes:"
-git remote -v
-
-echo "Fetching upstream:"
+UPSTRM="https://github.com/deeplearning4j/dl4j-examples"
+echo "Syncing fork to $UPSTRM"
+#
+#echo "Current remotes:"
+#git remote -v
+#
+echo "Adding remote..."
+git remote add upstream "$UPSTRM"
+#
+#echo "Current remotes:"
+#git remote -v
+#
+echo "Fetching upstream..."
 git fetch upstream
-
-echo "Switching to master branch:"
+#
+echo "Switching to master branch..."
 git checkout master
-
-echo "Merging upstream:"
-git merge upstream/master
-
-echo "Pushing to remote:"
+#
+echo "Merging upstream..."
+git merge upstream/master -m "merge upstream"
+#
+echo "Pushing to remote..."
 git push
-
+#
 echo ""
 read -rsp $'Done. Press any key to quit...\n' -n1 key
 
